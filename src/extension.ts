0c4d8c19fed4e938ceb7d6d915799c7915d60cd2
vikas <vp103808@gmail.com>
2023-09-07 16:35:33 +0530

    Infisical Env File Loader in Terminal

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,89 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { InfisicalData } from "./helpers/infisical.api";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "infisical-env-loader" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "infisical-env-loader" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "infisical-env-loader.loadToken",
+    async () => {
+      // The code you place here will be executed every time your command is executed
+      // Get Token from User
+      try {
+        const token = await vscode.window.showInputBox({
+          placeHolder: "Please enter Infisical Project Service Token here",
+          title: "Infisical Project Service Token",
+          validateInput(value) {
+            if (!value || value === "") {
+              return "Please Enter Valid Token";
+            }
+            return null;
+          },
+        });
+        if (!token) {
+          return;
+        }
+        const infisical = new InfisicalData(token);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('infisical-env-loader.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from infisical-env-loader!');
-	});
+        await vscode.window.withProgress(
+          {
+            location: vscode.ProgressLocation.Notification,
+            title: "Loading data",
+          },
+          async () => {
+            await infisical.getEnvironments().catch((e) => {
+              vscode.window.showErrorMessage(
+                "Failed to fetch data please check your token"
+              );
+              throw e;
+            });
+          }
+        );
+        //   console.log(scopes);
+        const pickedItem = await vscode.window.showQuickPick(
+          infisical.getQuickPickItems(),
+          {
+            canPickMany: false,
+            matchOnDetail: true,
+            placeHolder: "Choose Environment",
+            title: "Environment",
+          }
+        );
 
-	context.subscriptions.push(disposable);
+        if (!pickedItem) {
+          return;
+        }
+        await vscode.window.withProgress(
+          {
+            location: vscode.ProgressLocation.Notification,
+            title: "Loading data",
+          },
+          async () => {
+            await infisical.getEnvFile(pickedItem?.label).catch((e) => {
+              vscode.window.showErrorMessage("Failed to fetch data");
+              throw e;
+            });
+          }
+        );
+        infisical.loadEnvInTerminal();
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
---
+++ src/helpers/infisical.api.ts
@@ -0,0 +1,128 @@
+import * as crypto from "crypto";
+import axios from "axios";
+import * as vscode from "vscode";
+
+interface DecryptType {
+  ciphertext: string;
+  iv: string;
+  tag: string;
+  secret: string;
+}
+
+export class InfisicalData {
+  public serviceTokenData: any;
+  private serviceToken: string;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  private BASE_URL: string = "https://app.infisical.com";
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  private ALGORITHM: string = "aes-256-gcm";
+  private secrets: { [key: string]: string | undefined | null } = {};
+
+  constructor(public token: string) {
+    this.serviceToken = token;
+  }
+
+  async getEnvironments() {
+    // 1. Get your Infisical Token data
+    const response = await axios.get(`${this.BASE_URL}/api/v2/service-token`, {
+      headers: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        Authorization: `Bearer ${this.serviceToken}`,
+      },
+    });
+    if (response.status === 200) {
+      this.serviceTokenData = response.data;
+    }
+  }
+
+  getQuickPickItems() {
+    const { scopes } = this.serviceTokenData;
+    let items: Array<vscode.QuickPickItem> = [];
+    for (const scope of scopes) {
+      items.push({
+        label: scope.environment,
+        description: scope.secretPath,
+        picked: scopes[0].environment === scope.environment,
+      });
+    }
+
+    return items;
+  }
+
+  async getEnvFile(environment: string) {
+    const { workspace, encryptedKey, iv, tag } = this.serviceTokenData;
+    const serviceTokenSecret = this.serviceToken.substring(
+      this.serviceToken.lastIndexOf(".") + 1
+    );
+
+    const response = await axios.get(
+      `${this.BASE_URL}/api/v3/secrets?${new URLSearchParams({
+        environment: environment,
+        workspaceId: workspace,
+      })}`,
+      {
+        headers: {
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          Authorization: `Bearer ${this.serviceToken}`,
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      
+      const encryptedSecrets = response.data.secrets;
+      const projectKey = this.decrypt({
+        ciphertext: encryptedKey,
+        iv: iv,
+        tag: tag,
+        secret: serviceTokenSecret,
+      });
+
+      encryptedSecrets.map((secret: any) => {
+        const secretKey = this.decrypt({
+          ciphertext: secret.secretKeyCiphertext,
+          iv: secret.secretKeyIV,
+          tag: secret.secretKeyTag,
+          secret: projectKey,
+        });
+
+        const secretValue = this.decrypt({
+          ciphertext: secret.secretValueCiphertext,
+          iv: secret.secretValueIV,
+          tag: secret.secretValueTag,
+          secret: projectKey,
+        });
+
+        this.secrets = {
+          ...this.secrets,
+          [secretKey]: secretValue,
+        };
+      });
+    }
+  }
+
+  private decrypt({ ciphertext, iv, tag, secret }: DecryptType) {
+    const decipher = crypto.createDecipheriv(
+      this.ALGORITHM,
+      secret,
+      Buffer.from(iv, "base64")
+    ) as unknown as crypto.DecipherCCM;
+    decipher.setAuthTag(Buffer.from(tag, "base64"));
+
+    let cleartext = decipher.update(ciphertext, "base64", "utf8");
+    cleartext += decipher.final("utf8");
+
+    return cleartext;
+  }
+
+  loadEnvInTerminal() {
+    vscode.window.createTerminal({
+      env: this.secrets,
+      name: "Infisical Env Loader",
+      message: `Don't Close this Terminal`,
+      cwd: ".",
+    });
+
+    vscode.window.showInformationMessage("Env File Loaded SuccessFully");
+  }
+}
